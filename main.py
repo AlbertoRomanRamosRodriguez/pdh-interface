6da16023ec9ff8d94af87cb2b9881ac5ffa52ea6
Alberto R Ramos <rr.albertoroman@gmail.com>
2023-03-05 16:09:41 -0500

    exporting information from the database

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World")
+from PIL import Image
+
+import pandas as pd
+import requests
+import shutil
+import os
+
+IMAGE_PATH = '../pdh/media/Images/'
+
+def export_info():
+    res = requests.get('http://0.0.0.0:8000/ofundus/')
+    data = res.json()
+    df = pd.DataFrame(data)
+    images = df['image'].tolist()
+
+    folder = 'output'
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+    os.mkdir(folder)
+
+    for img in images:
+        img = os.path.join(IMAGE_PATH, img.split('/')[-1])
+        shutil.copy(src=img, dst=folder)
+
+    csv_path = os.path.join(folder, 'received.csv')
+    df.to_csv(csv_path)
